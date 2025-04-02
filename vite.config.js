e775b0f1d3d1ddca040a8ae6628869e98e58fdb1
fisicagames <rafaelj3d@gmail.com>
2025-04-02 16:03:31 -0300

    chore: Return Vite.js config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite';
-import terser from '@rollup/plugin-terser';
 
 export default defineConfig({
   build: {
-    minify: 'terser', // Usa Terser para minificação avançada
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console.log
-        drop_debugger: true, // Remove debugger
-        passes: 3, // Aplica múltiplas otimizações
-      },
-      mangle: {
-        properties: true, // Renomeia propriedades privadas
-        toplevel: true, // Minifica variáveis globais
-      },
-      format: {
-        comments: false, // Remove comentários
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks: {
@@ -25,10 +9,6 @@ export default defineConfig({
           'core': ['@babylonjs/core'],
         },
       },
-      plugins: [terser()],
     },
-  },
-  server: {
-    compress: true, // Habilita Gzip para servir arquivos menores
-  },
+  }
 });
